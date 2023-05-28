dad43856f2d705a937b45995e2d5ca7632d60442
tornikee <tornikexoxiashvili@gmail.com>
2023-05-28 14:37:09 +0400

    change

--- src/main/java/com/epam/rd/autotasks/CycleSwap.java
+++ src/main/java/com/epam/rd/autotasks/CycleSwap.java
@@ -2,10 +2,20 @@ package com.epam.rd.autotasks;
 
 class CycleSwap {
     static void cycleSwap(int[] array) {
-
+        int lastElement = array[array.length - 1];
+        for (int i = array.length - 1; i > 0; i--) {
+            array[i] = array[i - 1];
+        }
+        array[0] = lastElement;
     }
 
     static void cycleSwap(int[] array, int shift) {
-
+        shift =shift % array.length;
+        int[] ar=new int[shift];
+        System.arraycopy(array,array.length-shift,ar,0,shift);
+        for (int i=array.length-shift-1;i>=0;i--){
+            array[i+shift]=array[i];
+        }
+        System.arraycopy(ar,0,array,0,shift);
     }
 }
